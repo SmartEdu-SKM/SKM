e497c99444b70027b48eadc3f8ae65db23e28151
Kamya Batra <kamyabatra@gmail.com>
2015-12-18 01:32:04 +0530

    Added the feature of adding marks

---
+++ app/src/main/java/com/example/dell/smartedu/AddMarks.java
@@ -0,0 +1,115 @@
+package com.example.dell.smartedu;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
+
+import java.util.List;
+
+public class AddMarks extends BaseActivity {
+
+    String studentId;
+    String classId;
+    String sub;
+    TextView subject;
+    EditText examDesc;
+    EditText marksObtained;
+    EditText outOf;
+    Button addMarks;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_marks);
+
+        Bundle from_stud_result = getIntent().getExtras();
+        studentId = from_stud_result.getString("studentId");
+        classId = from_stud_result.getString("classId");
+
+
+
+        examDesc = (EditText) findViewById(R.id.examDesc);
+        marksObtained = (EditText) findViewById(R.id.marksDesc);
+        outOf = (EditText) findViewById(R.id.outOfDesc);
+        addMarks = (Button) findViewById(R.id.addMarks);
+        subject= (TextView) findViewById(R.id.subjectText);
+
+        final ParseObject[] classRef = new ParseObject[1];
+        ParseQuery<ParseObject> classQuery = ParseQuery.getQuery("Class");
+        classQuery.whereEqualTo("objectId", classId);
+        classQuery.findInBackground(new FindCallback<ParseObject>() {
+            public void done(List<ParseObject> classListRet, ParseException e) {
+                if (e == null) {
+                    classRef[0] = classListRet.get(0);
+                    sub = classRef[0].getString("subject");
+                    subject.setText(sub);
+
+                    final ParseObject[] studentRef = new ParseObject[1];
+                    ParseQuery<ParseObject> studentQuery = ParseQuery.getQuery("Student");
+                    studentQuery.whereEqualTo("class", classRef[0]);
+                    studentQuery.whereEqualTo("objectId", studentId);
+                    studentQuery.findInBackground(new FindCallback<ParseObject>() {
+                        public void done(List<ParseObject> studentListRet, ParseException e) {
+                            if (e == null) {
+                                studentRef[0] = studentListRet.get(0);
+
+                                addMarks.setOnClickListener(new View.OnClickListener() {
+                                    @Override
+                                    public void onClick(View v) {
+                                        if (examDesc.equals("") || marksObtained.equals("") || outOf.equals("")) {
+                                            Toast.makeText(getApplicationContext(), "Marks details cannot be empty!", Toast.LENGTH_LONG).show();
+                                        } else {
+                                            ParseObject marks = new ParseObject("Marks");
+                                            marks.put("student", studentRef[0]);
+                                            marks.put("class", classRef[0]);
+                                            marks.put("exam", examDesc.getText().toString());
+                                            marks.put("marksObtained", Float.parseFloat(marksObtained.getText().toString()));
+                                            marks.put("outOf", Float.parseFloat(outOf.getText().toString()));
+                                            marks.put("subject",sub);
+
+                                            marks.saveEventually();
+
+                                            Toast.makeText(getApplicationContext(), "Marks details added!", Toast.LENGTH_LONG).show();
+
+                                            new student_result();
+                                            finish();
+
+                                        }
+                                    }
+
+                                });
+
+
+
+                            } else {
+                                Toast.makeText(AddMarks.this, "error", Toast.LENGTH_LONG).show();
+                                Log.d("user", "Error: " + e.getMessage());
+                            }
+                        }
+                    });
+
+
+                } else {
+                    Toast.makeText(AddMarks.this, "error", Toast.LENGTH_LONG).show();
+                    Log.d("user", "Error: " + e.getMessage());
+                }
+
+
+
+            }
+        });
+
+
+    }
+
+
+}
