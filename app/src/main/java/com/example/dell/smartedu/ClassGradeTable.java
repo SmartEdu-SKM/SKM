ee066417a64e8298ad692362812df27c0c10b1e2
Shubham <shubham220@yahoo.in>
2016-02-03 23:49:58 +0530

    changes in database for classes and new section addition

---
+++ app/src/main/java/com/example/dell/smartedu/ClassGradeTable.java
@@ -0,0 +1,12 @@
+package com.example.dell.smartedu;
+
+/**
+ * Created by Shubham Bhasin on 03-Feb-16.
+ */
+public class ClassGradeTable {
+    public static final String TABLE_NAME="ClassGrade";
+    public static final String OBJECT_ID="objectId";
+    public static final String CLASS_GRADE="grade";
+    public static final String SECTION="section";
+    public static final String INSTITUTION="institution";
+  }
